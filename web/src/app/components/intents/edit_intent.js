81c288062b3e43b47e991304498150e795d67544
Geoffrey Poon <gpoon@lscm.hk>
2020-07-17 17:04:17 +0800

    bug fixed for entity highlighting and replicated entity in the same intent

--- web/src/app/components/intents/edit_intent.js
+++ web/src/app/components/intents/edit_intent.js
@@ -1,5 +1,9 @@
 angular.module('app').controller('EditIntentController', EditIntentController);
 
+String.prototype.replaceBetween = function(start, end, what) {
+  return this.substring(0, start) + what + this.substring(end);
+};
+
 function EditIntentController($rootScope, $scope, Bot, BotEntities, Intent, Expressions, Expression, Parameter, Parameters, Entities, Response, Response) {
   Bot.get({ bot_id: $scope.$routeParams.bot_id }, function (data) {
     $scope.bot = data;
@@ -13,12 +17,29 @@ function EditIntentController($rootScope, $scope, Bot, BotEntities, Intent, Expr
     $scope.intent = data;
   });
 
+  $scope._show_highlight = false;
+
+  function compare(a, b) {
+    const start_idx_a = a.parameter_start;
+    const start_idx_b = b.parameter_start;
+
+    let comparison = 0;
+    if(start_idx_a>start_idx_b){
+      comparison = -1;
+    }else if(start_idx_a<start_idx_b){
+      comparison = 1;
+    }
+    return comparison;
+  }
+
+
   loadExpressions();
 
   function loadExpressions() {
     Expressions.query({ intent_id: $scope.$routeParams.intent_id }, function (data) {
       $scope.expressionList = data;
-      loadExpressionParameters();
+      //console.log(data);
+      loadExpressionParameters2();
     });
   }
 
@@ -32,14 +53,47 @@ function EditIntentController($rootScope, $scope, Bot, BotEntities, Intent, Expr
       for (let z = 0; z <= $scope.expressionList.length; z++) {
         if ($scope.expressionList[z] !== undefined) {
           let text = $scope.expressionList[z].expression_text;
+          let htext = text;
           for (let i = 0; i <= data.length - 1; i++) {
             if (
               $scope.expressionList[z].expression_id === data[i].expression_id
             ) {
-              text = highlight(text, data[i].parameter_value);
+              //htext = highlight(text, data[i].parameter_value);
+              htext = highlight2(htext, data[i].parameter_value, data[i].parameter_start, data[i].parameter_end);
             }
           }
-          $scope.expressionList[z].expression_highlighted_text = text;
+          $scope.expressionList[z].expression_highlighted_text = htext;
+        }
+      }
+    });
+  }
+
+  function loadExpressionParameters2() {
+    Parameters.query({ intent_id: $scope.$routeParams.intent_id }, function (
+        data
+    ) {
+      $scope.parameterList = data;
+      $scope.parameterFilterList = data;
+      //Loop through each parameter and highlight the words it is for
+      for (let z = 0; z <= $scope.expressionList.length; z++) {
+        if ($scope.expressionList[z] !== undefined) {
+          let text = $scope.expressionList[z].expression_text;
+          let htext = text;
+          if(!$scope._show_highlight) {
+            let tmp = [];
+            for (let i = 0; i <= data.length - 1; i++) {
+              if (
+                  $scope.expressionList[z].expression_id === data[i].expression_id
+              ) {
+                tmp.push(data[i]);
+              }
+            }
+            tmp.sort(compare);
+            for (let i = 0; i <= tmp.length - 1; i++) {
+              htext = highlight2(htext, tmp[i].parameter_value, tmp[i].parameter_start, tmp[i].parameter_end);
+            }
+          }
+          $scope.expressionList[z].expression_highlighted_text = htext;
         }
       }
     });
@@ -80,6 +134,19 @@ function EditIntentController($rootScope, $scope, Bot, BotEntities, Intent, Expr
     return highlighted;
   }
 
+
+  function highlight2(str, word, start, end) {
+    const highlighted = str.replaceBetween(
+        start, end,
+        '<span style="padding: 3px; background-color: ' +
+        $scope.pastelColors() +
+        '">' +
+        word +
+        "</span>"
+    );
+    return highlighted;
+  }
+
   $scope.toggleArrow = function (expression_id) {
     if ($('#table_expression_' + expression_id).hasClass('show')) {
       $('#icon_expression_' + expression_id)
@@ -92,15 +159,90 @@ function EditIntentController($rootScope, $scope, Bot, BotEntities, Intent, Expr
     }
   };
 
+  var markSelection = (function() {
+    var markerTextChar = "\ufeff";
+    var markerTextCharEntity = "&#xfeff;";
+
+    var markerEl, markerId = "sel_" + new Date().getTime() + "_" + Math.random().toString().substr(2);
+
+    var selectionEl;
+
+    return function(win) {
+      win = win || window;
+      var doc = win.document;
+      var sel, range;
+      // Branch for IE <= 8
+      if (doc.selection && doc.selection.createRange) {
+        // Clone the TextRange and collapse
+        range = doc.selection.createRange().duplicate();
+        range.collapse(false);
+
+        // Create the marker element containing a single invisible character by creating literal HTML and insert it
+        range.pasteHTML('<span id="' + markerId + '" style="position: relative;">' + markerTextCharEntity + '</span>');
+        markerEl = doc.getElementById(markerId);
+      } else if (win.getSelection) {
+        sel = win.getSelection();
+        range = sel.getRangeAt(0).cloneRange();
+        range.collapse(false);
+
+        // Create the marker element containing a single invisible character using DOM methods and insert it
+        markerEl = doc.createElement("span");
+        markerEl.id = markerId;
+        markerEl.appendChild( doc.createTextNode(markerTextChar) );
+        range.insertNode(markerEl);
+      }
+
+      if (markerEl) {
+        // Lazily create element to be placed next to the selection
+        if (!selectionEl) {
+          selectionEl = doc.createElement("div");
+          selectionEl.style.border = "solid darkblue 1px";
+          selectionEl.style.backgroundColor = "lightgoldenrodyellow";
+          selectionEl.innerHTML = "&lt;- selection";
+          selectionEl.style.position = "absolute";
+
+          doc.body.appendChild(selectionEl);
+        }
+
+        // Find markerEl position http://www.quirksmode.org/js/findpos.html
+        var obj = markerEl;
+        var left = 0, top = 0;
+        do {
+          left += obj.offsetLeft;
+          top += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+
+        // Move the button into place.
+        // Substitute your jQuery stuff in here
+        selectionEl.style.left = left + "px";
+        selectionEl.style.top = top + "px";
+
+        markerEl.parentNode.removeChild(markerEl);
+      }
+    };
+  })();
 
   $scope.addParameter = function (expression_id) {
+    if($scope._show_highlight){
+      alert("Please switch to editing mode first!");
+      return false;
+    }
+
+    var selection = window.getSelection();
+    var start = selection.anchorOffset;
+    var end = selection.focusOffset;
+    //console.log(start);console.log(end);
+    //console.log(window.getSelection());
     const selectedText = window.getSelection().toString();
     if (selectedText !== '') {
-      const expressionText = $('#expression_' + expression_id).text();
+      //const expressionText = $('#expression_' + expression_id).text();
       const newObj = {};
+      //console.log(expressionText.indexOf(selectedText));
       newObj.expression_id = expression_id;
-      newObj.parameter_start = expressionText.indexOf(selectedText);
-      newObj.parameter_end = newObj.parameter_start + selectedText.length;
+      //newObj.parameter_start = expressionText.indexOf(selectedText);
+      //newObj.parameter_end = newObj.parameter_start + selectedText.length;
+      newObj.parameter_start = start;
+      newObj.parameter_end = end;
       newObj.parameter_value = selectedText;
       newObj.intent_id = Number($scope.$routeParams.intent_id);
       Parameter.save(newObj).$promise.then(function () {
@@ -145,5 +287,11 @@ function EditIntentController($rootScope, $scope, Bot, BotEntities, Intent, Expr
         loadExpressions();
       }
     );
+  };
+
+  $scope.toggleHighLight = function (v){
+    $scope._show_highlight = v;
+    //console.log($scope._show_highlight);
+    loadExpressions();
   };
 }
